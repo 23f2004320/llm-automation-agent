1bc800a2a4c451337b9a9a0752813fd713b2241a
Thakur vishalkumar vinod <23f2004320@ds.study.iitm.ac.in>
2025-02-16 02:00:30 +0530

    Refactor tasksB.py: remove obsolete file and add new automation functions; update Dockerfile and .env for consistency

--- Dockerfile
+++ Dockerfile
@@ -31,3 +31,4 @@ EXPOSE 8000
 
 # Run the FastAPI app
 CMD ["uvicorn", "app.app:app", "--host", "0.0.0.0", "--port", "8000"]
+
--- data/app.py
+++ data/app.py
@@ -19,8 +19,8 @@
 from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import PlainTextResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from app.tasksA import *
-from app.tasksB import *
+from data.tasksA import *
+from data.tasksB import *
 import requests
 from dotenv import load_dotenv
 import os
