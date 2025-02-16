181798538521bcb6d9b2748349ac4327b7a11206
Thakur vishalkumar vinod <23f2004320@ds.study.iitm.ac.in>
2025-02-16 10:45:20 +0530

    Refactor tasksB.py: replace obsolete file with new implementation; update Dockerfile for command consistency

--- app.py
+++ app.py
@@ -19,8 +19,8 @@
 from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import PlainTextResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from data.tasksA import *
-from data.tasksB import *
+from tasksA import *
+from tasksB import *
 import requests
 from dotenv import load_dotenv
 import os
